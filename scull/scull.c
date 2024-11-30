8425cc00dcdd8085063f1b7b257877cf507e5407
MufaroDKaseke <mufarodkaseke@gmail.com>
2024-11-30 20:43:31 +0200

    Updated

--- scull/scull.c
+++ scull/scull.c
@@ -1,5 +1,4 @@
-// charenc.c: Creates a char device that encrypts using Caesar Cipher using a key of one.
-// Decrypts the cipher text on closing the file.
+// Character Driver
 
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -7,10 +6,9 @@
 #include <asm/uaccess.h>
 #include <linux/loop.h>
 
-
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mufaro D Kaseke");
-MODULE_DESCRIPTION("A Simple Character Device Driver for openEuler");
+MODULE_DESCRIPTION("A simple character device driver for an openEuler assignment");
 MODULE_VERSION("0.1");
 
 int init_module(void);
@@ -21,16 +19,16 @@ static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
 #define SUCCESS 0
-#define DEVICE_NAME "charenc"
+#define DEVICE_NAME "mufarodev"
 #define BUF_LEN 85
 
 static int major;
-static int device_open = 0;
+static int dev_open = 0;
 static char msg[BUF_LEN];
-static char *msg_ptr;
+static char *msg_Ptr;
 
 // These are the sys calls available for this character driver
-static struct file_operations fops = {,
+static struct file_operations fops = {
   .owner = THIS_MODULE,
 	.read = device_read,
 	.write = device_write,
@@ -69,11 +67,11 @@ void cleanup_module(void)
 // When a user program opens the device file this function is called
 static int device_open(struct inode *inode, struct file *filp)
 {
-	if (device_open)
+	if (dev_open)
 		return -EBUSY;
 
-	device_open++;
-	msg_ptr = msg;
+	dev_open++;
+	msg_Ptr = msg;
 
 	// Increment the reference count of the module
 	try_module_get(THIS_MODULE);
@@ -84,9 +82,13 @@ static int device_open(struct inode *inode, struct file *filp)
 // When a user program closes the device file this function is called
 static int device_release(struct inode *inode, struct file *filp)
 {
+
+	//sprintf(msg, buf);
+
 	// Decrement the reference count of the module
-	device_open--;
+	dev_open--;
 	module_put(THIS_MODULE);
+
 	return SUCCESS;
 }
 
@@ -96,13 +98,13 @@ static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_
 	int bytes_read = 0;
 
 	// If we reach the eof we return 0
-	if (*msg_ptr == 0)
+	if (*msg_Ptr == 0)
 		return 0;
 
-	while (length && *msg_ptr) 
+	while (length && *msg_Ptr) 
 	{
 		// put_user transfers data from kernel data segment to the user data segment
-		put_user(*(msg_ptr++), buffer++);
+		put_user(*(msg_Ptr++), buffer++);
 		length--;
 		bytes_read++;
 	}
@@ -114,9 +116,14 @@ static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_
 // When a user program writes to the device file this function is called
 static ssize_t device_write(struct file *filp,const char *buff, size_t len, loff_t *off)
 {
-	buf[i]='\0';
-	// writing the string to the device
-	sprintf(msg, buf);
+	if (len > BUF_LEN -1)
+    return -EINVAL;
 
+  if (copy_from_user(msg, buff, len))
+    return -EFAULT;
+
+	// writing the encrypted string into the device
+	//sprintf(msg, buf);
+  msg[len]='\0';
 	return len;
 }
